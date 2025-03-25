c940e4583fd79e19a9fd77655e950c004d87d61f
maddiesolis <madelinesdeo@gmail.com>
2025-03-25 12:41:53 -0700

    chakra provider cleanup

--- portfolio-website/src/app/page.tsx
+++ portfolio-website/src/app/page.tsx
@@ -1,16 +1,48 @@
-import { Box, Button, Heading, Link } from '@chakra-ui/react'
+'use client'
 
-export default function Home() {
+import { Button, CloseButton, Drawer, IconButton, Portal, Text } from '@chakra-ui/react'
+import { useState } from 'react'
+import { BiMenu } from 'react-icons/bi'
+
+export const Home = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <Box p={4}>
-      <Button variant={'subtle'} colorPalette={'brand.primary'}>
-        Click me
-      </Button>
-      <Heading color={'brand.primary.subtle'}>Hello world</Heading>
-      <Link colorPalette={'brand.primary'} href={'#'}>
-        Learn more
-      </Link>
-      <Heading color={'brand.secondary.400'}>Hello world</Heading>
-    </Box>
+    <Drawer.Root open={open} onOpenChange={e => setOpen(e.open)}>
+      <Drawer.Trigger asChild>
+        <IconButton aria-label="open navigation menu" variant="ghost" size="lg">
+          <BiMenu />
+        </IconButton>
+      </Drawer.Trigger>
+      <Portal>
+        <Drawer.Backdrop />
+        <Drawer.Positioner>
+          <Drawer.Content p="10">
+            <Drawer.Header>
+              <Drawer.Title>Drawer Title</Drawer.Title>
+            </Drawer.Header>
+            <Drawer.Body>
+              <Text textStyle="md">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua.
+              </Text>
+            </Drawer.Body>
+            <Drawer.Footer>
+              <Button variant="outline" p="3" colorPalette={'brand.primary'}>
+                Cancel
+              </Button>
+              <Button p="3" colorPalette={'brand.primary'}>
+                Save
+              </Button>
+            </Drawer.Footer>
+            <Drawer.CloseTrigger asChild>
+              <CloseButton size="sm" />
+            </Drawer.CloseTrigger>
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Portal>
+    </Drawer.Root>
   )
 }
+
+export default Home
--- portfolio-website/src/components/ui/provider.tsx
+++ portfolio-website/src/components/ui/provider.tsx
@@ -1,13 +1,12 @@
 'use client'
 
 import { ChakraProvider } from '@chakra-ui/react'
-import { ColorModeProvider, type ColorModeProviderProps } from './color-mode'
 import customSystem from '../../../theme'
 
-export function Provider(props: ColorModeProviderProps) {
-  return (
-    <ChakraProvider value={customSystem}>
-      <ColorModeProvider {...props} />
-    </ChakraProvider>
-  )
+interface ProviderProps {
+  children: React.ReactNode
+}
+
+export function Provider({ children }: ProviderProps) {
+  return <ChakraProvider value={customSystem}>{children}</ChakraProvider>
 }
