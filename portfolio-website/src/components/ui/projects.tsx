809f9565b728698820e5651b4d7bff832aae4d53
maddiesolis <madelinesdeo@gmail.com>
2025-03-26 15:43:14 -0700

    projects components

--- portfolio-website/src/components/ui/projects.tsx
+++ portfolio-website/src/components/ui/projects.tsx
@@ -1,29 +1,80 @@
-import { Button, Card, Image, Text } from '@chakra-ui/react'
+import { Box, Card, Image, Tag, Wrap } from '@chakra-ui/react'
 import { FC } from 'react'
 import { ProjectProps } from './props'
-import { Provider } from './provider'
+import { CustomIconButton } from './icon-button'
+import { FaGithub, FaGlobe, FaLinkedin } from 'react-icons/fa'
 
-export const Project: FC<ProjectProps> = () => {
+const Project: FC<ProjectProps> = ({
+  title,
+  description,
+  technologies,
+  imageUrl,
+  websiteUrl,
+  githubUrl,
+  linkedInUrl,
+}) => {
   return (
     <Card.Root maxW="sm" overflow="hidden">
-      <Image
-        src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-        alt="Green double couch with wooden legs"
-      />
-      <Card.Body gap="2">
-        <Card.Title>Living room Sofa</Card.Title>
-        <Card.Description>
-          This sofa is perfect for modern tropical spaces, baroque inspired spaces.
-        </Card.Description>
-        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
-          $450
-        </Text>
+      <Image src={imageUrl} alt="Project Preview Image" aspectRatio={5 / 3} />
+      <Card.Body gap="2" alignItems={'center'}>
+        <Card.Title>{title}</Card.Title>
+        <Box>
+          {technologies.map((technology, index) => (
+            <Tag.Root key={index}>
+              <Tag.Label>{technology}</Tag.Label>
+            </Tag.Root>
+          ))}
+        </Box>
+        <Card.Description>{description}</Card.Description>
       </Card.Body>
-      <Card.Footer gap="2">
-        <Button>My app button</Button>
-
-        <Button variant="ghost">Add to cart</Button>
+      <Card.Footer gap="2" justifyContent="center">
+        {websiteUrl && (
+          <CustomIconButton
+            aria-label="link to live project"
+            onClick={() => window.open(websiteUrl, '_blank')}
+            color="black"
+            _hover={{
+              color: 'gray.500',
+            }}
+          >
+            <FaGlobe />
+          </CustomIconButton>
+        )}
+        {githubUrl && (
+          <CustomIconButton
+            aria-label="link to github repository"
+            onClick={() => window.open(githubUrl, '_blank')}
+            color="black"
+            _hover={{
+              color: 'gray.500',
+            }}
+          >
+            <FaGithub />
+          </CustomIconButton>
+        )}
+        {linkedInUrl && (
+          <CustomIconButton
+            aria-label="link to linkedin post"
+            onClick={() => window.open(linkedInUrl, '_blank')}
+            color="black"
+            _hover={{
+              color: 'gray.500',
+            }}
+          >
+            <FaLinkedin />
+          </CustomIconButton>
+        )}
       </Card.Footer>
     </Card.Root>
+  )
+}
+
+export const Projects: FC<{ projects: ProjectProps[] }> = ({ projects }) => {
+  return (
+    <Wrap>
+      {projects.map((project, index) => (
+        <Project key={index} {...project} />
+      ))}
+    </Wrap>
   )
 }
