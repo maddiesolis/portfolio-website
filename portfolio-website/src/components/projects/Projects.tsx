64b2c7a6afb0ce4c7398ebd0bce3030180c6935d
maddiesolis <madelinesdeo@gmail.com>
2025-04-09 01:20:57 -0700

    fix project cards heights

--- portfolio-website/src/components/projects/Projects.tsx
+++ portfolio-website/src/components/projects/Projects.tsx
@@ -21,7 +21,11 @@ export const ProjectsGrid: FC<{ projects: ProjectProps[] }> = ({ projects }) => 
   return (
     <SimpleGrid columns={{ base: 1, md: 1, lg: 2 }} gap={{ base: 4, md: 5, lg: 6 }}>
       {projects.map((project, index) => (
-        <Box key={index} animation={`${slideIn} 0.4s ease-in-out ${index * 0.3}s both`}>
+        <Box
+          key={index}
+          display={'flex'}
+          animation={`${slideIn} 0.4s ease-in-out ${index * 0.3}s both`}
+        >
           <Project {...project} />
         </Box>
       ))}
