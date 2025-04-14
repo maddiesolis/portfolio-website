b371814cfcc752164fa9bc7ca755a65678ff4d9a
maddiesolis <madelinesdeo@gmail.com>
2025-04-14 03:55:38 -0700

    favicon

--- portfolio-website/src/app/layout.tsx
+++ portfolio-website/src/app/layout.tsx
@@ -8,6 +8,16 @@ const dmSans = DM_Sans({
   subsets: ['latin'],
 })
 
+export const metadata = {
+  title: 'Madeline Solis de Ovando',
+  description: 'Portfolio website',
+  icons: {
+    icon: '/favicon.png',
+    shortcut: '/favicon.png',
+    apple: '/favicon.png',
+  },
+}
+
 export default function RootLayout({
   children,
 }: Readonly<{
